272c6153ab45ab239854395119955f638fd97724
Dk-234 <2300033814@kluniversity.in>
2025-03-31 23:53:59 +0530

    updated

--- server/config/environment.js
+++ server/config/environment.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   PORT: process.env.PORT || 5000,
-  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/MahaLoot',
+  MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://dibakarpal234:5RN7sRQNAmDp6sGO@mahaloot.af6yod8.mongodb.net/',
   JWT_SECRET: process.env.JWT_SECRET || 'your_jwt_secret_key',
   JWT_EXPIRE: process.env.JWT_EXPIRE || '7d',
   REDIS_URL: process.env.REDIS_URL || 'redis://localhost:6379',
